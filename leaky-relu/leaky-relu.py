4560f161c8e5c304bb090aa15cf361dcfd3ff70e
Utkarsh Mishra <utkarsh07022000@gmail.com>
2026-09-04 08:50:40 +0530

    Solve leaky-relu

---
+++ leaky-relu/leaky-relu.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def leaky_relu(x: list | float, alpha: float = 0.01) -> np.ndarray:
+    """
+    Returns elementwise Leaky ReLU values as a NumPy array matching the input shape.
+    """
+    # Write code here
+    x = np.asarray(x)
+    return np.asarray(np.maximum(alpha*x,x))
